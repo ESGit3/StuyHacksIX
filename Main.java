8385f17c58d9ff5517056df618627e5e9deb8571
ESGit3 <ethanshan706@gmail.com>
2020-01-03 11:05:24 -0500

    basic jframe

--- Main.java
+++ Main.java
@@ -1,5 +1,14 @@
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.GraphicsConfiguration;
+
+
 public class Main {
+    static GraphicsConfiguration gc;
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame frame = new JFrame(gc);
+        frame.setVisible(true);
     }
 }
