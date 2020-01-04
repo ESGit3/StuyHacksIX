5c9a2a7bc7bf46c35c870ee3c2a630a7e113a6f8
TechnoRazor <technorazor@hotmail.com>
2020-01-04 14:53:26 -0500

    src

--- src/org/seq/tasks/TaskManager.java
+++ src/org/seq/tasks/TaskManager.java
@@ -1,12 +1,14 @@
 package org.seq.tasks;
 
+import java.time.LocalDateTime;
+
 import org.seq.tasks.Task.*;
 import org.seq.tasks.Task.RepeatableTask.*;
 
 public class TaskManager {
 
 	public static void main(String[] args) {
-		WeekdayTask wt = new WeekdayTask(null, null, 0b1111111);
+		WeekdayTask wt = new WeekdayTask(LocalDateTime.now(), LocalDateTime.now().plusHours(1), 0b1111111);
 		
 	}
 
