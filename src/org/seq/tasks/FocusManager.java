f171c201dd42211ee26fc47784b21f0e33e07544
ESGit3 <ethanshan706@gmail.com>
2020-01-07 15:55:52 -0500

    added windows kill, probably doesnt work, yusuf pls check

--- src/org/seq/tasks/FocusManager.java
+++ src/org/seq/tasks/FocusManager.java
@@ -1,16 +1,9 @@
 package org.seq.tasks;
 
 import java.awt.*;
-import java.awt.event.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.management.ManagementFactory;
-
-import com.sun.jna.Native;
-import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef.HWND;
-import com.sun.jna.platform.win32.WinDef.RECT;
 
 public class FocusManager {
 
@@ -22,18 +15,11 @@ public class FocusManager {
 		return (OS.contains("mac"));
 	}
 
-	public static void main(String[] args) throws AWTException, InterruptedException, IOException {
-		//if currentWindow = something on the blacklist, alt f4
-//		HWND hwnd = User32.INSTANCE.GetActiveWindow();
+	public static void main(String[] args) throws IOException {
+		ProcessBuilder processBuilder = new ProcessBuilder();
 		if (isWindows()) {
-            System.out.println("win");
-		}
-		if(isMac()) {
-			ProcessBuilder processBuilder = new ProcessBuilder();
-			processBuilder.command("bash", "-c", "pgrep Spotify | head -1");
+			processBuilder.command("cmd.exe", "/C", "pgrep Spotify | head -1");
 			var pidFromName = processBuilder.start();
-//			pidFromName = pidFromName.replace("Process[pid=", "");
-//			pidFromName = pidFromName.replace(pidFromName.substring(pidFromName.lastIndexOf(",")), "");
 			try (var reader = new BufferedReader(
 					new InputStreamReader(pidFromName.getInputStream()))) {
 
@@ -47,12 +33,22 @@ public class FocusManager {
 			}
 			System.out.println("mac");
 		}
-//		Robot robot = new Robot();
-//		Thread.sleep(1000);
-//		robot.keyPress(KeyEvent.VK_ALT);
-//		robot.keyPress(KeyEvent.VK_F4);
-//		robot.keyRelease(KeyEvent.VK_F4);
-//		robot.keyRelease(KeyEvent.VK_ALT);
-	}
 
+		if(isMac()) {
+			processBuilder.command("bash", "-c", "pgrep Spotify | head -1");
+			var pidFromName = processBuilder.start();
+			try (var reader = new BufferedReader(
+					new InputStreamReader(pidFromName.getInputStream()))) {
+
+				String line;
+
+				while ((line = reader.readLine()) != null) {
+					line = "kill " + line;
+					System.out.println(line);
+					Runtime.getRuntime().exec(line);
+				}
+			}
+			System.out.println("mac");
+		}
+	}
 }
